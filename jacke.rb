3d3343da3321435722cba70531bcb90fc705d92c
Stan <iamjacke@gmail.com>
2012-09-04 11:21:43 +0600

    beta phase

--- jacke.rb
+++ jacke.rb
@@ -31,7 +31,15 @@ protect do
    File.read(File.join('public', 'index.html'))
   end
 
-
+post '/signup' do
+  email = params[:email]
+  unless email.nil? || email.strip.empty?
+    mailchimp = Hominid::API.new(settings.mailchimp_api_key)
+    list_id = mailchimp.find_list_id_by_name(settings.mailchimp_list_name)
+    raise "Unable to retrieve list id from MailChimp API." unless list_id
+    mailchimp.list_subscribe(list_id, email, {}, 'html', false, true, true, false)
+  end
+  "Success."
 
 =begin
 post '/sendmail' do
